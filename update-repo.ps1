ce4afa2157b4fe3b766bf075f8b1f170ce6a4521
HeXDeMoN <Hexdemon@gmail.com>
2021-01-24 00:58:46 -0600

    arrrg

--- update-repo.ps1
+++ update-repo.ps1
@@ -4,6 +4,7 @@ $file = (Get-ChildItem .\latestaddons.xml)
 $hash = (Get-FileHash -Path $file.FullName -Algorithm MD5).hash
 Set-Content -Path ($file.FullName + '.md5') -Value $hash -Force
 
+<# Kodi doesnt like compress-archive find alternative
 $DirToCompress=".\repository.hexdemon"
 $TempDir = "$env:TEMP\repository.hexdemon"
 $ZipFile=".\repository.hexdemon\repository.hexdemon-$($version).zip"
@@ -12,3 +13,4 @@ Remove-Item -Force $TempDir
 New-Item $TempDir -ItemType "directory"
 Get-ChildItem $DirToCompress -Exclude '*.zip' | Copy-Item -Destination $TempDir -Force
 Compress-Archive $TempDir -DestinationPath $ZipFile
+#>
