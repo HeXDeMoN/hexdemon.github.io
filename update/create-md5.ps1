b8b9d16b4d957cd8b293440252dbc7f41cbe070b
HeXDeMoN <Hexdemon@gmail.com>
2021-01-23 22:49:56 -0600

    Update create-md5.ps1

--- update/create-md5.ps1
+++ update/create-md5.ps1
@@ -1,3 +1,4 @@
-$file = '.\addons.xml'
-$hash = (Get-FileHash -Path $file -Algorithm MD5).hash
-Set-Content -Path ($file + '.md5') -Value $hash -Force
+$file = (Get-ChildItem .\addons.xml)
+$hash = (Get-FileHash -Path $file.FullName -Algorithm MD5).hash
+Set-Content -Path ($file.FullName + '.md5') -Value $hash -Force
+
