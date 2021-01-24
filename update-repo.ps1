569deb2ad39e103d5882e9036747be5939373de6
HeXDeMoN <Hexdemon@gmail.com>
2021-01-24 00:04:10 -0600

    reorganized repo

---
+++ update-repo.ps1
@@ -0,0 +1,14 @@
+$version = '1.1'
+
+$file = (Get-ChildItem .\addons.xml)
+$hash = (Get-FileHash -Path $file.FullName -Algorithm MD5).hash
+Set-Content -Path ($file.BaseName + '.md5') -Value $hash -Force
+
+$DirToCompress=".\repository.hexdemon"
+$TempDir = "$env:TEMP\repository.hexdemon"
+$ZipFile=".\repository.hexdemon\repository.hexdemon-$($version).zip"
+
+Remove-Item -Force $TempDir
+New-Item $TempDir -ItemType "directory"
+Get-ChildItem $DirToCompress -Exclude '*.zip' | Copy-Item -Destination $TempDir -Force
+Compress-Archive $TempDir -DestinationPath $ZipFile
--- update/create-md5.ps1
+++
@@ -1,4 +0,0 @@
-$file = (Get-ChildItem .\addons.xml)
-$hash = (Get-FileHash -Path $file.FullName -Algorithm MD5).hash
-Set-Content -Path ($file.BaseName + '.md5') -Value $hash -Force
-
